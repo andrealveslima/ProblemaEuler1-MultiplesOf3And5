bc7f76b101a4db98ea830d7b2600127b26a1379a
andrealveslima <andrealveslimeira@gmail.com>
2013-08-28 16:26:21 +0200

    Reorganizando arquivos e postando solucao simples

---
+++ ProblemaEuler1-MultiplesOf3And5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemaEuler1_MultiplesOf3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int somaDosMultiplos = 0;
+
+            // Para cada número entre 1 e 999.
+            for (int contador = 1; contador < 1000; contador++)
+                // Se o número for múltiplo de 3 ou 5, acrescentar no sumarizador.
+                if (contador % 3 == 0 || contador % 5 == 0)
+                    somaDosMultiplos += contador;
+
+            Console.WriteLine("A soma de todos os múltiplos de 3 ou 5 entre 1 e 999 é {0}", somaDosMultiplos);
+            Console.ReadLine();
+        }
+    }
+}
