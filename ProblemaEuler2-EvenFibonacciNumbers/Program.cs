c15cebd1b2fad8d4caae289c524f8a21300ee5ff
andrealveslima <andrealveslimeira@gmail.com>
2013-08-29 13:25:24 +0200

    Prob 2, solucao simples - achar melhor alternat.

---
+++ ProblemaEuler2-EvenFibonacciNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProblemaEuler2_EvenFibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float termoMaximo = 4000000;
+            float termoAnterior = 1;
+            float termoAtual = 1;
+            float somaDosTermosPares = 0;
+
+            while (termoAtual < termoMaximo)
+            {
+                float novoTermo = termoAnterior + termoAtual;
+
+                termoAnterior = termoAtual;
+                termoAtual = novoTermo;
+
+                if (termoAtual % 2 == 0)
+                    somaDosTermosPares += termoAtual;
+            }
+
+            Console.WriteLine("A soma dos elementos pares da sequência de Fibonacci entre 1 e 4 milhoes é: {0}", somaDosTermosPares);
+            Console.ReadLine();
+        }
+    }
+}
